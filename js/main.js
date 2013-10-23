c932c0027ec41f7399b8c1512bd6b26d2ef78524
DaveRand <davef@edulinksys.com>
2013-10-22 17:40:52 -0700

    Commit6

--- js/main.js
+++ js/main.js
@@ -8,24 +8,20 @@ var app = {
         }
     },
 
-    findByName: function() {
-        var self = this;
-        this.store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
+    registerEvents: function() {
+        $('body').on('mousedown', 'a', function(event) {
+            $(event.target).addClass('tappable-active');
         });
-    },
-
-    renderHomeView: function() {
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+        $('body').on('mouseup', 'a', function(event) {
+            $(event.target).removeClass('tappable-active');
+        });
     },
 
     initialize: function() {
         var self = this;
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+        this.registerEvents();
         this.store = new MemoryStore(function() {
-            self.renderHomeView();
+            $('body').html(new HomeView(self.store).render().el);
         });
     }
 
