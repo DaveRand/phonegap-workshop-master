19e7c2a17373a273113e3a15f15115cd72abae77
DaveRand <davef@edulinksys.com>
2013-10-22 17:28:07 -0700

    Commit Part 5

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
 var app = {
 
-//    findByName: function() {
-//        console.log('findByName');
-//        this.store.findByName($('.search-key').val(), function(employees) {
-//            var l = employees.length;
-//            var e;
-//            $('.employee-list').empty();
-//            for (var i=0; i<l; i++) {
-//                e = employees[i];
-//                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-//            }
-//        });
-//    },
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+
     findByName: function() {
         var self = this;
         this.store.findByName($('.search-key').val(), function(employees) {
-                              $('.employee-list').html(self.employeeLiTpl(employees));
-                              });
+            $('.employee-list').html(self.employeeLiTpl(employees));
+        });
+    },
+
+    renderHomeView: function() {
+        $('body').html(this.homeTpl());
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
 
     initialize: function() {
@@ -24,32 +25,9 @@ var app = {
         this.homeTpl = Handlebars.compile($("#home-tpl").html());
         this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
         this.store = new MemoryStore(function() {
-                                     self.renderHomeView();
-                                     });
-    },
-    
-    showAlert: function (message, title) {
-    if (navigator.notification) {
-        navigator.notification.alert(message, null, title, 'OK');
-    } else {
-        alert(title ? (title + ": " + message) : message);
-    },
-    
-    renderHomeView: function() {
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+            self.renderHomeView();
+        });
     }
-//    renderHomeView: function() {
-//        var html =
-//        "<div class='header'><h1>Home</h1></div>" +
-//        "<div class='search-view'>" +
-//        "<input class='search-key'/>" +
-//        "<ul class='employee-list'></ul>" +
-//        "</div>"
-//        $('body').html(html);
-//        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-//    }
-},
 
 };
 
